e6f5d25486c9adcfee2270629c473105c340df8a
i-net /// software <tools@inetsoftware.de>
2017-12-18 15:08:03 +0100

    Make the plugin aware of the CSP nonce.

--- script.js
+++ script.js
@@ -571,6 +571,7 @@ function PhosphorPlayer(bindto_id){
 
     var addCSSRule = (function(style){
         var head = document.head || jQuery("head")[0] || document.documentElement;
+        if ( JSINFO['nonce'] ) { style.setAttribute('nonce', JSINFO['nonce']); }
         var sheet = head.appendChild(style).sheet;
         return function(selector, css){
             var propText = Object.keys(css).map(function(p){
